d2e5ad53515adf9c1811315629acd0da78c3df20
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-06-09 13:20:57 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,7 @@ pip3 install pyTelegramBotApi
 pip3 install telebot 
 pip3 install my_fake_useragent 
 pip3 install types 
+pip3 install phonenumbers
+pip3 install bs4
+pip3 install progress
+pip3 install smsactivateru
