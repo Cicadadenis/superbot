c79077a740d2bb9be49f9e87c6e230e7aa767e63
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-06-09 13:00:08 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 apt update
 apt install python3.9 -y
 apt install python3-pip -y
-pip3 install pyTelegramBotApi -y
-pip3 install telebot -y
-pip3 install my_fake_useragent -y
+pip3 install pyTelegramBotApi 
+pip3 install telebot 
+pip3 install my_fake_useragent 
 pip3 install types 
