076e270f0464c49d09ce3f460a2dba0874190366
Cicada 3301 <50529176+Cicadadenis@users.noreply.github.com>
2021-06-09 14:12:58 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 apt update
 apt install python3.9 -y
 apt install python3-pip -y
+python3 -m venv tutorial-env
+source tutorial-env/bin/activate
+pip3 install pipenv
+pipenv --python 3
 pip3 install pyTelegramBotApi 
 pip3 install telebot 
 pip3 install my_fake_useragent 
